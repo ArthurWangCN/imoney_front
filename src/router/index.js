21b0ec1acc564e5b4ae36474a00c24aa89ebd59c
ArthurWangCN <legendaryarthur@163.com>
2022-02-04 00:27:38 +0800

    登录注册页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
-import Home from '@/container/Home'
-import Data from '@/container/Data'
-import User from '@/container/User'
+import Home from '@/container/Home/index.jsx'
+import Data from '@/container/Data/index.jsx'
+import User from '@/container/User/index.jsx'
+import Login from '@/container/Login/index.jsx'
 
 const routes = [
   {
@@ -14,6 +15,10 @@ const routes = [
   {
     path: "/user",
     component: User
+  },
+  {
+    path: "/login",
+    component: Login
   }
 ];
 
